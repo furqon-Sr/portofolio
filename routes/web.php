8609e5fc854c289d19351fe8179222388f0be2c6
furqon-sr <fhan111205@gmail.com>
2026-06-21 16:54:58 +0700

    debug: bypass all web middlewares for db-test and clean up public db-test file

--- routes/web.php
+++ routes/web.php
@@ -133,7 +133,11 @@ Route::get('/db-test', function () {
     return response()->json($results, 200, [], JSON_PRETTY_PRINT);
 })->withoutMiddleware([
     \Illuminate\Cookie\Middleware\EncryptCookies::class,
+    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
     \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
 ]);
 
 require __DIR__.'/auth.php';
