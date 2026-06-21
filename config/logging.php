19a2dc33a5825f6b0ea3d070d85fc60b7f267bd9
furqon-sr <fhan111205@gmail.com>
2026-06-21 17:11:50 +0700

    debug: set log channel to stderr on vercel

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', isset($_SERVER['VERCEL']) ? 'stderr' : 'stack'),
 
     /*
     |--------------------------------------------------------------------------
