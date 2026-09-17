2e5491a15eee5ae6ea82883379df88a62a726970
furqon-Sr <fhan111205@gmail.com>
2026-09-17 22:50:45 +0700

    chore: remove debug headers

--- public/index.php
+++ public/index.php
@@ -39,9 +39,6 @@ if ($isPublicCacheable) {
     header_remove('Set-Cookie');
 }
 
-$response->headers->set('X-Debug-Cacheable', $isPublicCacheable ? 'yes' : 'no');
-$response->headers->set('X-Debug-Req-Path', $path);
-
 $response->send();
 if ($isPublicCacheable) {
     header_remove('Set-Cookie');
